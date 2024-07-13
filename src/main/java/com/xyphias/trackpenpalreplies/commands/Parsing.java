5f18b065aefc8ff59602c8c9d7b47bfff73d5fe4
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2024-07-13 15:09:50 +0100

    Refactor parsing of A command to extract the details from the input and remove the hard coding

--- src/main/java/com/xyphias/trackpenpalreplies/commands/Parsing.java
+++ src/main/java/com/xyphias/trackpenpalreplies/commands/Parsing.java
@@ -4,18 +4,31 @@ import com.xyphias.trackpenpalreplies.Letter;
 import com.xyphias.trackpenpalreplies.Penpal;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Parsing {
     public static Command parse(String input) {
         if (input.equals("L")) return new ListLetters();
         
-        else if (input.startsWith("A")) 
+        else if (input.startsWith("A")) {
+            Matcher matcher = 
+                    Pattern.compile("A (\\w+);(\\d{2}/\\d{2}/\\d{4})")
+                            .matcher(input);
+            matcher.matches();
+            
+            String name = matcher.group(1);
+            LocalDate receivedOn = 
+                    LocalDate.parse(
+                            matcher.group(2),
+                            DateTimeFormatter.ofPattern("dd/MM/yyyy")
+                    );
+            
             return new AddLetter(
-                    new Letter(
-                            new Penpal("Amandine"),
-                            LocalDate.of(2024, 7, 4)
-                    )
+                    new Letter(new Penpal(name), receivedOn)
             );
+        }
 
         return new Quit();
     }
