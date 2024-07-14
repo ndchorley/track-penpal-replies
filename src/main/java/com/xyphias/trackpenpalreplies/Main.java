7ba8ddd13377a63bad8263ea552294d10452328b
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2024-07-14 14:03:34 +0100

    Inline a variable in Main

--- src/main/java/com/xyphias/trackpenpalreplies/Main.java
+++ src/main/java/com/xyphias/trackpenpalreplies/Main.java
@@ -1,17 +1,14 @@
 package com.xyphias.trackpenpalreplies;
 
 import com.xyphias.trackpenpalreplies.io.ConsoleOutputWriter;
-import com.xyphias.trackpenpalreplies.io.OutputWriter;
 
 public class Main {
     public static void main(String[] args) {
-        OutputWriter consoleOutputWriter = new ConsoleOutputWriter();
-        
         TrackPenpalReplies app =
                 new TrackPenpalReplies(
                         new InMemoryLetterBox(),
                         System.console()::readLine,
-                        consoleOutputWriter
+                        new ConsoleOutputWriter()
                 );
 
         app.run();
