5e3d2d8e858a373d244d66a345a09db7c566c8de
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2024-07-14 09:22:10 +0100

    Add another behaviour to the contract for LetterBox

--- src/test/java/com/xyphias/trackpenpalreplies/unit/LetterBoxContract.java
+++ src/test/java/com/xyphias/trackpenpalreplies/unit/LetterBoxContract.java
@@ -1,16 +1,33 @@
 package com.xyphias.trackpenpalreplies.unit;
 
+import com.xyphias.trackpenpalreplies.Letter;
 import com.xyphias.trackpenpalreplies.LetterBox;
+import com.xyphias.trackpenpalreplies.Penpal;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public abstract class LetterBoxContract {
     protected LetterBox letterBox;
-    
+
     @Test
     public void a_new_letter_box_is_empty() {
         assertThat(letterBox.isEmpty()).isTrue();
         assertThat(letterBox.contents().isEmpty()).isTrue();
+    }
+
+    @Test
+    public void it_stores_a_letter_that_is_added() {
+        Letter letter =
+                new Letter(
+                        new Penpal("Dmitry"),
+                        LocalDate.of(2024, 5, 1)
+                );
+        
+        letterBox.add(letter);
+
+        assertThat(letterBox.contents().getFirst()).isEqualTo(letter);
     }
 }
