d228748e249b4ef4257f39e16b695aa5c1ad469c
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2024-07-14 14:09:58 +0100

    Wire up SQLiteLetterBox in Main

--- src/main/java/com/xyphias/trackpenpalreplies/Main.java
+++ src/main/java/com/xyphias/trackpenpalreplies/Main.java
@@ -1,16 +1,34 @@
 package com.xyphias.trackpenpalreplies;
 
 import com.xyphias.trackpenpalreplies.io.ConsoleOutputWriter;
+import org.flywaydb.core.Flyway;
 
 public class Main {
     public static void main(String[] args) {
+        LetterBox letterBox = 
+                createSQLiteLetterBox(
+                        System.getenv("HOME") + "/penpal_letterbox.db"
+                );
+        
         TrackPenpalReplies app =
                 new TrackPenpalReplies(
-                        new InMemoryLetterBox(),
+                        letterBox,
                         System.console()::readLine,
                         new ConsoleOutputWriter()
                 );
 
         app.run();
+    }
+
+    private static LetterBox createSQLiteLetterBox(String dbFile) {
+        String jdbcUrl = "jdbc:sqlite:" + dbFile;
+
+        Flyway
+                .configure()
+                .dataSource(jdbcUrl, "", "")
+                .load()
+                .migrate();
+        
+        return new SQLiteLetterBox(dbFile);
     }
 }
