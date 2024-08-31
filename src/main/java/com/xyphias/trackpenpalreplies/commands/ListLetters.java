1480723adad758ba18d75423dc480b03e67d4fdf
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2024-08-31 10:37:33 +0100

    More refactoring to raise the level of abstraction in ListLetters::execute

--- src/main/java/com/xyphias/trackpenpalreplies/commands/ListLetters.java
+++ src/main/java/com/xyphias/trackpenpalreplies/commands/ListLetters.java
@@ -6,6 +6,7 @@ import com.xyphias.trackpenpalreplies.infrastructure.io.OutputWriter;
 
 import java.time.format.DateTimeFormatter;
 import java.util.Comparator;
+import java.util.stream.Stream;
 
 public final class ListLetters implements Command {
     private final LetterBox letterBox;
@@ -18,14 +19,19 @@ public final class ListLetters implements Command {
 
     public void execute() {
         if (letterBox.isEmpty())
-            outputWriter.writeLine("No letters need a reply");
+            displayNoRepliesNeededMessage();
         else
-            letterBox.contents()
-                    .stream()
-                    .sorted(byReceivedOn)
-                    .forEach(this::displayDetails);
+            allLetters().sorted(byReceivedOn).forEach(this::displayDetails);
     }
 
+    private void displayNoRepliesNeededMessage() {
+        outputWriter.writeLine("No letters need a reply");
+    }
+
+    private Stream<Letter> allLetters() {
+        return letterBox.contents().stream();
+    }
+    
     private final Comparator<Letter> byReceivedOn = Comparator.comparing(Letter::receivedOn);
 
     private void displayDetails(Letter letter) {
