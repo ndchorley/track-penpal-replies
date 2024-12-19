21834d5fb0a05300abe60969075d45e17a4738aa
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2024-12-19 10:09:59 +0000

    Upgrade Flyway to 11.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 
 dependencies {
     implementation("org.xerial:sqlite-jdbc:3.47.1.0")
-    implementation("org.flywaydb:flyway-core:10.21.0")
+    implementation("org.flywaydb:flyway-core:11.1.0")
     
     implementation("org.slf4j:slf4j-api:2.0.16")
     implementation("org.slf4j:slf4j-jdk14:2.0.16")
