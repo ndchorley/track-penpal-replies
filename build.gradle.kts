ea62f2295f93baa3996e7b904e0a875829197e17
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2025-04-15 19:43:28 +0100

    Upgrade ApprovalTests to 24.20.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("org.assertj:assertj-core:3.27.3")
-    testImplementation("com.approvaltests:approvaltests:24.19.0")
+    testImplementation("com.approvaltests:approvaltests:24.20.0")
 }
 
 tasks.test {
