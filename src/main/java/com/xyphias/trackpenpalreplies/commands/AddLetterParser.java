e7e395a5b66ec30a50feec44a437085759e4f451
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2025-05-09 12:59:42 +0100

    Introduce AddLetterParser

---
+++ src/main/java/com/xyphias/trackpenpalreplies/commands/AddLetterParser.java
@@ -0,0 +1,38 @@
+package com.xyphias.trackpenpalreplies.commands;
+
+import com.xyphias.trackpenpalreplies.Letter;
+import com.xyphias.trackpenpalreplies.LetterBox;
+import com.xyphias.trackpenpalreplies.Penpal;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class AddLetterParser {
+    private final LetterBox letterBox;
+
+    public AddLetterParser(LetterBox letterBox) {
+        this.letterBox = letterBox;
+    }
+
+    public AddLetter parse(String input) {
+        Matcher matcher =
+                Pattern.compile("A ([\\w ]+);(\\d{2}/\\d{2}/\\d{4})")
+                        .matcher(input);
+
+        if (!matcher.matches()) return null;
+
+        String name = matcher.group(1);
+        LocalDate receivedOn =
+                LocalDate.parse(
+                        matcher.group(2),
+                        DateTimeFormatter.ofPattern("dd/MM/yyyy")
+                );
+
+        return new AddLetter(
+                new Letter(new Penpal(name), receivedOn),
+                letterBox
+        );
+    }
+}
--- src/main/java/com/xyphias/trackpenpalreplies/commands/CommandFactory.java
+++ src/main/java/com/xyphias/trackpenpalreplies/commands/CommandFactory.java
@@ -1,12 +1,9 @@
 package com.xyphias.trackpenpalreplies.commands;
 
-import com.xyphias.trackpenpalreplies.Letter;
 import com.xyphias.trackpenpalreplies.LetterBox;
 import com.xyphias.trackpenpalreplies.Penpal;
 import com.xyphias.trackpenpalreplies.infrastructure.io.OutputWriter;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -23,23 +20,7 @@ public class CommandFactory {
         if (input.equals("L")) return new ListLetters(letterBox, outputWriter);
         
         else if (input.startsWith("A")) {
-            Matcher matcher = 
-                    Pattern.compile("A ([\\w ]+);(\\d{2}/\\d{2}/\\d{4})")
-                            .matcher(input);
-            
-            if (!matcher.matches()) return null;
-            
-            String name = matcher.group(1);
-            LocalDate receivedOn = 
-                    LocalDate.parse(
-                            matcher.group(2),
-                            DateTimeFormatter.ofPattern("dd/MM/yyyy")
-                    );
-            
-            return new AddLetter(
-                    new Letter(new Penpal(name), receivedOn),
-                    letterBox
-            );
+            return new AddLetterParser(letterBox).parse(input);
         } else if (input.startsWith("R")) {
             Matcher matcher = Pattern.compile("R ([\\w]+)").matcher(input);
             matcher.matches();
