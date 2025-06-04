ea64eacadd21e781d5212d2a51d77b1928d86e64
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2025-06-04 07:42:15 +0100

    Use a generic type parameter for ResultConveniences::successful, instead of String

--- src/test/java/com/xyphias/trackpenpalreplies/ResultConveniences.java
+++ src/test/java/com/xyphias/trackpenpalreplies/ResultConveniences.java
@@ -5,11 +5,11 @@ import com.xyphias.trackpenpalreplies.foundational.Result;
 import com.xyphias.trackpenpalreplies.foundational.Success;
 
 public class ResultConveniences {
-    public static <T> T successful(Result<String, T> result) {
+    public static <E, T> T successful(Result<E, T> result) {
         return switch (result) {
             case Success(T value) -> value;
-            case Failure(_) -> 
-                    throw new IllegalStateException("Expected a Success, but found a Failure");
+            case Failure(E error) -> 
+                    throw new IllegalStateException("Expected a Success, but found a Failure: " + error);
         };
     }
 }
