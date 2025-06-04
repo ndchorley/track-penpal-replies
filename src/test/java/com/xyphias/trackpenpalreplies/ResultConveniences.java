ad3bace6b82f00512d86b12ae51b2535b8db4f18
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2025-06-04 07:43:19 +0100

    Use V for type parameter of ResultConveniences::successful, for consistency with Result

--- src/test/java/com/xyphias/trackpenpalreplies/ResultConveniences.java
+++ src/test/java/com/xyphias/trackpenpalreplies/ResultConveniences.java
@@ -5,9 +5,9 @@ import com.xyphias.trackpenpalreplies.foundational.Result;
 import com.xyphias.trackpenpalreplies.foundational.Success;
 
 public class ResultConveniences {
-    public static <E, T> T successful(Result<E, T> result) {
+    public static <E, V> V successful(Result<E, V> result) {
         return switch (result) {
-            case Success(T value) -> value;
+            case Success(V value) -> value;
             case Failure(E error) -> 
                     throw new IllegalStateException("Expected a Success, but found a Failure: " + error);
         };
