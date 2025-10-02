4e105b8d523e4d27a0a091f02a837568e05b28c0
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2025-10-02 07:42:02 +0100

    Upgrade Shadow to 9.2.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java")
     id("application")
-    id("com.gradleup.shadow").version("9.1.0")
+    id("com.gradleup.shadow").version("9.2.2")
     id("com.adarshr.test-logger").version("4.0.0")
 }
 
