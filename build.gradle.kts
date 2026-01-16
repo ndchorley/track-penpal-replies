26c28e875f97fa275d18709ae595edd4f10b9c52
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2026-01-16 08:10:14 +0000

    Upgrade ApprovalTests

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("org.assertj:assertj-core:3.27.6")
-    testImplementation("com.approvaltests:approvaltests:26.2.0")
+    testImplementation("com.approvaltests:approvaltests:26.4.0")
 }
 
 tasks.test {
