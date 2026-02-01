665d3a3c96fb3dfe670231740ae80b24da69adc1
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2026-02-01 10:33:08 +0000

    Upgrade Flyway

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 
 dependencies {
     implementation("org.xerial:sqlite-jdbc:3.51.1.0")
-    implementation("org.flywaydb:flyway-core:11.20.2")
+    implementation("org.flywaydb:flyway-core:12.0.0")
     
     val slf4JVersion = "2.0.17"
     implementation("org.slf4j:slf4j-api:$slf4JVersion")
