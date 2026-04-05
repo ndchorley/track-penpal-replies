90b69caf97618d7150b4822997748f00e180e39c
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2026-04-05 16:09:57 +0100

    Replace another approval test

--- src/test/java/com/xyphias/trackpenpalreplies/functional/TrackPenpalRepliesTests.java
+++ src/test/java/com/xyphias/trackpenpalreplies/functional/TrackPenpalRepliesTests.java
@@ -11,9 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -48,7 +46,10 @@ public class TrackPenpalRepliesTests {
 
         app.run();
 
-        Approvals.verify(allOutput(outputWriter));
+        assertThat(lastOutputExceptThePrompt(outputWriter))
+                .isEqualTo("""
+                        Amandine, 04 July 2024
+                        """);
     }
 
     @Test
